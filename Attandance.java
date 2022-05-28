90cb3de1cad0c71bc0b386f989a06a72f12f2370
mukeshkapse <mukeshkapse1832@gmail.com>
2022-05-28 23:59:28 +0530

    Day 1 Attandance

---
+++ Attandance.java
@@ -0,0 +1,20 @@
+package Bridgelabz.Assigmnet.Day1;
+
+public class Attandance
+{
+	public static void main(String[] args) {
+		
+	
+	double Val = Math.random();
+
+	if(Val < 0.5)
+	{
+		System.out.println("Employee is Present");
+
+		}
+	else
+	{
+		System.out.println("Employee is Absent");
+	}
+
+}}
